5c757479e70f2d84bf9245a21f70dd411cf354ba
Blake Warnock <blakewarn21@gmail.com>
2024-10-31 12:10:37 -0500

    adds modules from projects

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 $(document).ready(function () {
-  $("#module-trigger").click(function () {
-    console.log("Clicked");
-    $("#module-container").css("display", "block");
-    $("body").css("overflow", "hidden");
-    $("#darkness").css({ opacity: ".5", display: "block" });
+  var id;
+
+  $(".module-trigger").click(function () {
+    id = $(this).attr("id");
+
+    $(".module-container").each(function () {
+      console.log("tigger id = " + id);
+      console.log("looped module id = " + $(this).attr("id"));
+
+      if (id == "resume-trigger" && $(this).attr("id") == "resume-module") {
+        $(this).css("display", "block");
+      }
+      if (id == "LTC" && $(this).attr("id") == "LTC-module") {
+        $(this).css("display", "block");
+      }
+      $("body").css("overflow", "hidden");
+      $("#darkness").css({ opacity: ".5", display: "block" });
+    });
   });
-  $("#module-close").click(function () {
+
+  $(".module-close").click(function () {
     location.reload();
   });
 });
